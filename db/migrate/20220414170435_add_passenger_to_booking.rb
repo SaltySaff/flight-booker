4516222fc9ab8e6bcfc83f21fc986601be709b6c
SaltySaff <danielalexander.ash@gmail.com>
2022-04-14 18:08:11 +0100

    Create booking and flight models

---
+++ db/migrate/20220414170435_add_passenger_to_booking.rb
@@ -0,0 +1,5 @@
+class AddPassengerToBooking < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :bookings, :passenger, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_03_043550) do
+ActiveRecord::Schema.define(version: 2022_04_14_170435) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "passenger_id"
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -30,6 +39,15 @@ ActiveRecord::Schema.define(version: 2022_04_03_043550) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "bookings", "passengers"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
