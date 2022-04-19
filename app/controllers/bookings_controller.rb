bc7886ef751a1bdc1f72d6d0e7eb3d64282b2470
SaltySaff <danielalexander.ash@gmail.com>
2022-04-19 20:20:23 +0100

    Add form to make booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,30 @@
 class BookingsController < ApplicationController
   def new
     @flight = Flight.where(id: flight_params[:flight]).first
-    p @flight
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
   end
 
   private
 
   def flight_params
-    params.permit(:flight, :commit)
+    params.permit(:flight)
+  end
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passenger_attributes: %i[id name email])
   end
 end
